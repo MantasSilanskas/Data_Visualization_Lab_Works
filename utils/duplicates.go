befcdaa1252b49f97fc5cc908b8a85390ab58a34
MantasSil <mantas.silanskas.ms@gmail.com>
2021-03-21 08:40:43 +0200

    BE: (#0004) database package renamed to db

--- utils/duplicates.go
+++ utils/duplicates.go
@@ -1,12 +1,12 @@
 package utils
 
 import (
-	"github.com/MantasSilanskas/Data_Visualization_Lab_Works/pkg/database"
+	"github.com/MantasSilanskas/Data_Visualization_Lab_Works/pkg/db"
 	"github.com/MantasSilanskas/Data_Visualization_Lab_Works/pkg/reader"
 )
 
 // IsFileAlreadyInDatabase checks if certain file already exist in database
-func IsFileAlreadyInDatabase(input reader.File, files []database.BSONFile) bool {
+func IsFileAlreadyInDatabase(input reader.File, files []db.BSONFile) bool {
 
 	for _, v := range files {
 		if input.Name == v.FileName {
