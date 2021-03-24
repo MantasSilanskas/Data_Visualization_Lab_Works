0ed46034ca6cf95d8bb891f144e3e99c9d82ec87
MantasSil <mantas.silanskas.ms@gmail.com>
2021-03-24 17:05:27 +0200

    BE: (#0006) added go.mod

---
+++ go.mod
@@ -0,0 +1,5 @@
+module data_visualization_lab_works
+
+go 1.13
+
+require github.com/go-echarts/go-echarts/v2 v2.2.3 // indirect
--- internal/server/server.go
+++ internal/server/server.go
@@ -1,8 +1,13 @@
 package server
 
 import (
+	"fmt"
 	"log"
 	"net/http"
+	"os"
+
+	"github.com/go-echarts/go-echarts/charts"
+	"github.com/go-echarts/go-echarts/go-echarts/v2/opts"
 )
 
 func Connection() error {
@@ -19,6 +24,18 @@ func Connection() error {
 	return nil
 }
 
-func HumidityHandler(w http.ResponseWriter, r *http.Request) {
+func HumidityHandler(w http.ResponseWriter, _ *http.Request) {
 
+	fmt.Println("hello")
+	nameItems := []string{"testas"}
+	bar := charts.NewBar()
+	bar.SetGlobalOptions(charts.WithTitleOpts(opts.Title{"Humidyti"}))
+	bar.SetXAxis(nameItems)
+	bar.ExtendYAxis(opts.YAxis{Name: "Mean", Data: []float32{5, 5}})
+	bar.ExtendYAxis(opts.YAxis{Name: "Count", Data: []int{5}})
+	f, err := os.Create("humidiry-bar.html")
+	if err != nil {
+		log.Println("Failed to create humidyti bar")
+	}
+	bar.Render(f)
 }
